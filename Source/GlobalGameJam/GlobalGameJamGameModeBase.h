9c928770e6285ea4245c92ce3b6b964e438068cd
skynnyng <skowlven@outlook.fr>
2023-01-10 18:12:45 +0100

    Init

---
+++ Source/GlobalGameJam/GlobalGameJamGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GlobalGameJamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLOBALGAMEJAM_API AGlobalGameJamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
